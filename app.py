ac66c92026e2bacfe95616ab88ae5dbf28801b0b
WeiS49 <swh_1C3@outlook.com>
2021-06-27 20:01:11 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 
 from flask import Flask
 from flask import escape, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+import click
+import os
+
 
 app = Flask(__name__)
+# 创建数据库连接地址
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app) # 加载配置(app.config)
 
-name = 'W S'
-movies = [
+import click
+
+@app.cli.command() # 使用装饰器将内容注册为命令
+@click.option('--drop', is_flag=True, help='Create after drop.') # 添加(布尔类型)参数
+def initdb(drop):
+    """ Initialize the database. """
+    if drop:   # 如果输入drop参数, 意为销毁当前表
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialize database.') # 打印提示内容.
+
+@app.cli.command()
+def forge():
+    """ Generate fake data. """
+    name = 'W S'
+    movies = [
     {'title': 'My Neighbor Totoro', 'year': '1988'},
     {'title': 'Dead Poets Society', 'year': '1989'},
     {'title': 'A Perfect World', 'year': '1993'},
@@ -17,10 +39,35 @@ movies = [
     {'title': 'WALL-E', 'year': '2008'},
     {'title': 'The Pork of Music', 'year': '2012'},
 ]
+    user = User(name=name)
+    db.session.add(user)
+    for movie in movies:
+        m = Movie(title=movie['title'], year=movie['year'])
+        db.session.add(m)
+        
+    db.session.commit()
+    click.echo('Done.')
+
+"""
+    1. 创建模型类(继承db.Model)
+    2. 声明内容(包括主键)
+"""
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
 
 @app.route('/')
 def index():
-    return render_template('index.html', name=name, movies=movies)
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html', user=user, movies=movies)
+    # return render_template('index.html', name=name, movies=movies)
     # return 'hello, world!'
 
 @app.route('/greeting')
