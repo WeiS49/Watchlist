b8f280e2817f8fa90118b9de6e83f70eefe44acb
WeiS49 <swh_1C3@outlook.com>
2021-06-27 15:44:58 +0800

    add render templates

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 
 from flask import Flask
-from flask import escape, url_for
+from flask import escape, url_for, render_template
 
 app = Flask(__name__)
 
+name = 'Grey Li'
+movies = [
+    {'title': 'My Neighbor Totoro', 'year': '1988'},
+    {'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'},
+]
+
 @app.route('/')
 def index():
-    return 'hello, world!'
+    return render_template('index.html', name=name, movies=movies)
+    # return 'hello, world!'
 
 @app.route('/greeting')
 def hello():
@@ -35,4 +50,7 @@ def test_url_for():
     print(f"输出: {url_for('user_page', name='mike')}")
     print(f"输出: {url_for('test_url_for')}", type(url_for('test_url_for')))
     return 'test page'
+
+
+
 
